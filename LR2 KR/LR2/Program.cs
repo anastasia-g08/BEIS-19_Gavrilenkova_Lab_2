571f2554e32916151b6fa20e6fa4da11268e085a
Anastasia <80090475+anastasia-g08@users.noreply.github.com>
2021-03-06 23:28:58 +0300

    Create LR2KR

---
+++ LR2 KR/LR2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LR2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите значение х = ");
+                double x = double.Parse(Console.ReadLine());
+                double f1, f2;
+                if (x >= 2)
+                {
+                    f1 = (1.0 / 7.0 + Math.Log(Math.Sqrt(x)));
+                    f2 = Math.Exp(Math.Sqrt(x - 2));
+                    Console.Write("Ответ: " +  f1*f2);
+
+                }
+                else
+                    Console.WriteLine("Введенное значение не входит в область допустимых значений");                
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\n\nВведенное значение некорректно");
+                Console.ReadKey();
+            }
+        }
+    }
+}
